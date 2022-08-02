3dea592c6828e7e63e40f9977e88b1064f5dc992
Shubham <106910292+Shubham8799@users.noreply.github.com>
2022-08-02 23:43:53 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+print("1. addition")
+operation = input("choose number:")
+if operation =="1":
+	num1 = input("enter first number : ")
+	num2 = input("enter second number :")
+	print(int(num1)+int(num2))
+elif operation == "2":
+     num1 = input("enter first number : ")
+	 num2 = input("enter second number :")
+	 print(int(num1)*int(num2))
+else:
+    pass print("invalid")
+    
