6e56ba0f4acc43745832f91ce3a7dc9ecd44598e
Shubham <106910292+Shubham8799@users.noreply.github.com>
2022-08-02 23:58:53 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,13 +1,12 @@
 print("1. addition")
 operation = input("choose number:")
 if operation =="1":
-	num1 = input("enter first number : ")
-	num2 = input("enter second number :")
-	print(int(num1)+int(num2))
+   num1 = input("enter first number : ")
+   num2 = input("enter second number :")
+   print(int(num1)+int(num2))
 elif operation == "2":
-     num1 = input("enter first number : ")
-	 num2 = input("enter second number :")
-	 print(int(num1)*int(num2))
+    num1 = input("enter first number : ")
+    num2 = input("enter second number :")
+    print(int(num1)*int(num2))
 else:
   print("invalid")
-    
